4bfbdddfeca8db60bb1fe4a305d1542f30af2868
yonde927 <svyuta1202@gmail.com>
2022-03-25 14:58:39 +0900

    push_front

--- include/conn/connector.h
+++ include/conn/connector.h
@@ -1,6 +1,7 @@
 #include <libssh/sftp.h>
 #include <libssh/libssh.h>
 #include <pthread.h>
+#include <sys/stat.h>
 
 typedef struct RemoteConnector
 {
@@ -8,3 +9,22 @@ typedef struct RemoteConnector
     sftp_session sftp;
     pthread_mutex_t mutex;
 } RemoteConnector;
+
+typedef enum FileType
+{
+    RegularFile,
+    Directory,
+    Other
+} FileType;
+
+typedef struct RemoteStat
+{
+    char path[256];
+    struct stat;
+    FileType filetype;
+} RemoteStat;
+
+RemoteConnector* GetRemoteConnector();
+
+int conn_stat(char* path,RemoteStat* remotestat);
+int conn_readdir(char* path);
